0babcf6d5647bff0aa2bac5da1185b6843f0e900
Batshoun <andrew.batshoun@accenturefederal.com>
2022-12-16 13:40:59 -0600

    changed home controller methods to string, to show only the view

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-insert into tasks(description, due_date)
-values('Study for exam', ''),
-	  ('Walk the dog', '11/22/2022'),
-	  ('Order books', '');
-
-insert into users(email, user_name, password)
-values ('jim@email.com', 'jim_e', 'abc123'),
-	   ('bob@email.com', 'bob_e', 'bob123');
